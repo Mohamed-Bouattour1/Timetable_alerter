0773f253227d01b4ea5f741aaf2747b1667a8d2f
mohamed bouattour <mohamedbouattourpro@gmail.com>
2025-04-13 20:31:06 +0200

    parser + nats client + recoverAndSendEvents function

--- scheduler/cmd/main.go
+++ scheduler/cmd/main.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"os"
 	"os/signal"
+	"scheduler/internal/helpers"
+	"scheduler/internal/natsclient"
+	"scheduler/internal/parser"
 	"time"
 
 	"github.com/sirupsen/logrus"
@@ -21,9 +24,43 @@ func main() {
 	<-quit
 
 	sc.Stop()
-	logrus.Info("⏹️ Scheduler arrêté.")
+	logrus.Info("Scheduler arrêté.")
 }
 func recoverAndSendEvents(ctx context.Context) {
-	logrus.Info("🔁 Lancement de recoverAndSendEvents()...")
-	// Ici on fera : appel API Config + récupération + parsing + envoi NATS
+	logrus.Info("Récupération et envoi des événements...")
+
+	// Récupérer toutes les resources depuis l'API Config
+	resources, err := helpers.GetResources("http://localhost:8080")
+	if err != nil {
+		logrus.Error("Échec récupération des resources :", err)
+		return
+	}
+
+	// Pour chaque resource, télécharger son .ics
+	for _, resource := range resources {
+		logrus.Infof("Traitement de la resource : %s (%s)", resource.Name, resource.ID)
+
+		rawData, err := helpers.DownloadICal(resource.URL)
+		if err != nil {
+			logrus.Errorf("Erreur téléchargement .ics pour %s", resource.Name)
+			continue
+		}
+
+		// Parser le contenu .ics pour obtenir une liste d'événements
+		events, err := parser.ParseICalToEvents(rawData, resource.ID)
+		if err != nil {
+			logrus.Errorf("Erreur parsing .ics pour %s", resource.Name)
+			continue
+		}
+
+		logrus.Infof("%d événements extraits pour %s", len(events), resource.Name)
+
+		// Publier chaque événement dans NATS
+		for _, e := range events {
+			err := natsclient.PublishEvent(e)
+			if err != nil {
+				logrus.Errorf("Échec publication NATS pour event %s", e.ID)
+			}
+		}
+	}
 }
